7f155260c05d91e31bfed7b48eb9c0e60a5fac0b
Chinmay Savadikar <savadikarc@gmail.com>
2020-12-05 13:27:27 +0530

    Initial commit

---
+++ configpy/parser.py
@@ -0,0 +1,86 @@
+import yaml
+from typing import Dict
+
+class AtomicConfig():
+
+    def __init__(self, attribute: Dict):
+        self.attribute = attribute
+
+    def __getattr__(self, key):
+        print(f'Trying to retrieve {key}')
+        attribute_value = self.attribute.get(key)
+        if attribute_value is None:
+            raise AttributeError(f'{self.attribute} has no attribute {key}')
+
+class Config():
+
+    def __init__(self, config_file):
+        self.config_file = config_file
+        self.load_yaml()
+        self.convert_config_dict(self.config_dict)
+
+    def __getattr__(self, key):
+
+        try:
+            print(f'Trying to rerieve {key}')
+            return getattr(self.config, key)
+        except AttributeError as e:
+            raise AttributeError(e)
+
+    def load_yaml(self):
+
+        with open(self.config_file, 'r') as f:
+            config_dict = yaml.safe_load(f)
+        self.config_dict = config_dict
+
+    def convert_config_dict(self, subconfig):
+        
+        # Outermost
+        if isinstance(subconfig, dict):
+            self.config = self.config_from_dict(subconfig)
+        elif isinstance(subconfig, list):
+            self.config = self.config_from_list(subconfig)
+        else:
+            self.config = subconfig
+
+    def config_from_dict(self, subconfig):
+
+        if isinstance(subconfig, dict):
+            # Use a dict to wrap a dict. Don't @ me.
+            temp_dict = {}
+            for key, value in subconfig.items():
+                # Convert value to atomic config
+                if isinstance(value, dict):
+                    config = self.config_from_dict(value)
+                else:
+                    config = value # Atomic value (str, int, float, etc.)
+                temp_dict[key] = config
+            # Wrap
+            wrapped_config = AtomicConfig(temp_dict)
+        else:
+            wrapped_config = subconfig
+        return wrapped_config
+
+    def config_from_list(self, subconfig):
+
+        if isinstance(subconfig, list):
+            # Use a list to wrap a list. Don't @ me.
+            temp_list = []
+            for em in subconfig:
+                if isinstance(em, list):
+                    config = self.config_from_list(em)
+                elif isinstance(em, dict):
+                    config = self.config_from_dict(em)
+                else:
+                    config = em
+                temp_list.append(config)
+
+
+if __name__ == '__main__':
+    config = Config('config.yml')
+    print(config.name)
+    # print(config.count, type(config.count))
+    # print(config.one, config.list.one.one_bar)
+    # c1 = AtomicConfig(4)
+    # c2 = AtomicConfig(c1, 'c1')
+    # print(c1.c1)
