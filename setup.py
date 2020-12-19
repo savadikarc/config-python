f8bc471b888f854ce30c9ff113679eb9569789d8
Chinmay Savadikar <savadikarc@gmail.com>
2020-12-19 16:35:01 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("Readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="configpy",
+    version="0.0.1",
+    author="Chinmay Savadikar",
+    author_email="savadikarc@gmail.com",
+    description="A wrapper to access file-based config as Python class attributes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/savadikarc/config-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+)
