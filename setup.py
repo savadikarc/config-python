28c368299748c11cdcaed03f181be7cfbabd875a
Chinmay Savadikar <savadikarc@gmail.com>
2020-12-19 17:24:31 +0530

    Dependencies in setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/savadikarc/config-python",
+    install_requires=['pyyaml>=5.3.1'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
