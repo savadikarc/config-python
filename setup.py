1d6c1319f2901099fb2debabb4f9354058b92dd0
Chinmay Savadikar <savadikarc@gmail.com>
2021-12-10 12:21:20 -0500

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/savadikarc/config-python",
-    install_requires=['pyyaml>=5.3.1'],
+    install_requires=['pyyaml>=5.4'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
